db1d72eaca5a4786ecc545680fb2e72274dc3764
okFox <smayfield@gmail.com>
2019-12-18 18:49:23 -0800

    1 custom route completed and basic CRUD completed

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 
-const mongoose = require('mongoose');
-const csv = require('csvtojson');
+// const mongoose = require('mongoose');
+// const csv = require('csvtojson');
 
-mongoose.connect('mongodb://localhost:27017/books', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-});
+// mongoose.connect('mongodb://localhost:27017/books', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+//   useCreateIndex: true
+// });
 
-const schema = new mongoose.Schema({
-  bookID: String,
-  title: String,
-  authors: String,
-  average_rating: String,
-  isbn: String,
-  isbn13: String,
-  language_code: String,
-  num_pages: Number,
-  ratings_count: Number,
-  text_reviews_count: Number 
-});
+// const schema = new mongoose.Schema({
+//   bookID: String,
+//   title: String,
+//   authors: String,
+//   average_rating: String,
+//   isbn: String,
+//   isbn13: String,
+//   language_code: String,
+//   num_pages: Number,
+//   ratings_count: Number,
+//   text_reviews_count: Number 
+// });
 
-const Book = mongoose.model('Book', schema);
+// const Book = mongoose.model('Book', schema);
 
-const csvFilePath = './csv/books.csv';
+// const csvFilePath = './csv/books.csv';
 
-csv()
-  .fromFile(csvFilePath)
-  .then((jsonObj)=>{
-    return Book.create(jsonObj);
+// csv()
+//   .fromFile(csvFilePath)
+//   .then((jsonObj)=>{
+//     return Book.create(jsonObj);
 
-  });
+//   });
 
